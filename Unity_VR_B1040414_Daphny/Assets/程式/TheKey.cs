5a3b0edd8eab2ca0ffdf26e620f76a152bc6ee3d
liermiersa.lisa@gmail.com <liermiersa.lisa@gmail.com>
2020-01-02 00:37:59 +0800

    完成

---
+++ Unity_VR_B1040414_Daphny/Assets/程式/TheKey.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TheKey : MonoBehaviour {
+
+
+
+    public void ThrowPickUp()
+    {
+        print("撿起物件");
+    }
+    public void ThrowDetach(GameObject obj)
+    {
+        print("放開物件");
+        Destroy(obj);
+    }
+    public void ThrowHeld()
+    {
+        print("拿住物件");
+    }
+}
